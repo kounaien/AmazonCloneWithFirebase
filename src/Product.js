13400e943c23ef880b336a463a464b8913e68b88
kounaien <1127fujihiroto@gmail.com>
2020-10-01 21:12:45 +0900

    reducer.js

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+  const [state, dispatch] = useStateValue();
+  const addToBasket = () => {
+    // dispatch the data
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -22,7 +38,7 @@ function Product({ title, image, price, rating }) {
 
       <img src={image} alt="" />
 
-      <button>Add to Busket</button>
+      <button onClick={addToBasket}>Add to Busket</button>
     </div>
   );
 }
--- src/reducer.js
+++ src/reducer.js
@@ -3,17 +3,17 @@ export const initialState = {
 };
 
 const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "ADD_TO_BASKET":
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
 
-    switch(action.type) {
-        case 'ADD_TO_BASKET';
-        return {
-            ...state, 
-            basket: [...state.basket, action.item],
-        };
-
-        default: 
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export default reducer;
